0607bd8547bc9b88680a08a0f30e64671895d2a8
legobeerus <goofyahwalking@gmail.com>
2025-12-26 16:59:03 +0100

    Addition of /anonymous-case command

---
+++ commands/anonymous-case.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("anonymous-case")
+    .setDescription("Submit a Sealed Case File to High Command.")
+    .addStringOption(option =>
+      option
+        .setName("link")
+        .setDescription("Link to the Case File")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+        option
+            .setName("casenumber")
+            .setDescription("The identifying number of the case")
+            .setRequired(true),
+    ),
+
+  async execute(interaction) {
+    const channel = "1449832209316839455";
+    await interaction.deferReply();
+
+    try {
+    
+    const embed = new EmbedBuilder()
+        .setTitle(`Sealed Case File #${casenumber}`)
+        .setDescription(`Submitted by ${interaction.user}`)
+        .addFields(
+                { name: "Case File", value: `[View Case File](${link})`, inline: false },
+        )
+        .setColor(0x00aff1)
+        .setTimestamp();
+
+        await channel.send({ embeds: [embed] });
+
+    } catch (err) {
+      console.error(err);
+      await interaction.editReply("⚠️ Something went wrong.");
+    }
+  }
+}
